2730d5b11f6c0d9b871c72aa033e2178faa27258
Dave Kanter <moatgoat@hotmail.com>
2018-06-17 13:54:29 -0700

    Fixed bug that caused server to immediately crash on launch

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running on http://${PORT}/`);
 });
