e951256b74d4e1c72b134e5c9aba0d29b37d28af
Dave Kanter <moatgoat@hotmail.com>
2018-06-19 13:06:36 -0700

    Trying to incorporate pg-error error messaging.

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const http = require('http');
+const { Client } = require('pg');
+const pgerror = require('pg-error');
+
+// because of the insanity of running Postgress
+// I have to involve a dev-focused error library...
+// ...another mouth to feed!
+
 
 // keep the port flexible to work with Heroku
 const PORT = process.env.PORT || 5000;
+const { DATABASE_URL } = process.env;
 
 const server = http.createServer((req, res) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Base code!\n');
+  client.connect()
+    .then(() => client.query('SELECT * FROM alltables'))
+    .then((result) => {
+      res.end(`${result.rows[0].name}\n`);
+      client.end();
+    })
+    .on("pgerror", function(err){
+      // cute error handling from the dev site
+      // why not?
+        switch (err.severity) {
+          case "ERROR" :
+          case "FATAL" :
+          case "PANIC" : return this.emit("oops-error", err)
+          default: return this.emit("oops-notice", err)
+      }
+    });
+  res.end(`Error: ${ err }\n`);
 });
 
 server.listen(PORT, () => {
